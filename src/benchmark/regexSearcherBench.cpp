51aef71b8efacbe892a292f13b1eb8573020f5f4
hakeris1010 <hakeris1010@gmail.com>
2018-01-13 19:25:45 +0200

    Regex benchmarked! Now the idea of developing a GrylloLexer is clear!

--- src/benchmark/regexSearcherBench.cpp
+++ src/benchmark/regexSearcherBench.cpp
@@ -4,9 +4,10 @@
 
 int main ()
 {
-    std::string reg = R"((\b[[:alpha:]]+\b)|(\b\d+\b))";
+    std::string reg = R"((\b[[:alpha:]]+\b)|(\b\d+\b)|(\s+)|(.))";
     std::regex r( reg );
-    std::string str = "foo bar 123";
+
+    std::string str = "foo bar 123[]  \nqvf";
     
     std::cout << "Target sequence: "<< str << std::endl;
     std::cout << "Regular expression: "<< reg << std::endl;
@@ -18,17 +19,18 @@ int main ()
                              ++i)
     {
         std::smatch m = *i;
-        std::cout << "\nMatch value: " << m.str() << " at Position " << m.position() << 
-                     ". Matches: "<< m.size() <<"\n";
+        std::cout << "\nMatch: " << m.str() << "\n Position " << m.position() << 
+                     ", Length: "<< m.length() <<", SubMatches: "<< m.size() <<"\n";
 
         for(auto index = 1; index < m.size(); ++index ){
             if (!m[index].str().empty()) {
-                std::cout << "Capture group ID: " << index-1 << std::endl;
+                std::cout << " Capture group ID: " << index-1 << "\n";
+                break;
             }
         }
     }  
 
-    std::cout<< "\n\nMatching with std::regex_search.\n";
+    /*std::cout<< "\n\nMatching with std::regex_search.\n";
     s = str;
     std::smatch m;
     while (std::regex_search (s, m, r)) {
@@ -40,7 +42,7 @@ int main ()
 
         std::cout << "\n";
         s = m.suffix().str();
-    } 
+    } */
 
     return 0;
 }
